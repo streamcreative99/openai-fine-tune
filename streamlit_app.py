e689204ffcb0d0af81f940b4b41ca2db644bc463
Brian Brinkman <brian@streamcreative.com>
2023-08-23 11:43:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,42 @@
 import streamlit as st
-from langchain.llms import OpenAI
+import openai
 
 # App title
-st.title('SC Starter Kit App')
+st.title('🤖🔎 OpenAI Fine-Tuning')
 
 # Check for OpenAI API Key in Streamlit's secrets
 if 'OPENAI_API_KEY' in st.secrets:
-    st.sidebar.success('API key successfully loaded from secrets!', icon='✅')
+    st.sidebar.success('API key successfully loaded from secrets!')
     openai_api_key = st.secrets['OPENAI_API_KEY']
 else:
     openai_api_key = st.sidebar.text_input('Enter OpenAI API Key:', type='password')
     if not openai_api_key.startswith('sk-'):
-        st.sidebar.warning('Please enter your OpenAI API key!', icon='⚠')
+        st.sidebar.warning('Please enter your OpenAI API key!')
+
+# File Uploader for Fine-Tuning
+uploaded_file = st.sidebar.file_uploader("Upload your training dataset (in .jsonl format)", type="jsonl")
+
+# Fine-Tuning Button
+if st.sidebar.button('Start Fine-Tuning'):
+    if uploaded_file and openai_api_key.startswith('sk-'):
+        # Upload the Dataset to OpenAI
+        uploaded = openai.File.create(file=uploaded_file, purpose='fine-tune')
+        
+        # Start the Fine-Tuning Job
+        openai.api_key = openai_api_key
+        response = openai.FineTuningJob.create(training_file=uploaded.id, model="gpt-3.5-turbo")
+        
+        if response:
+            st.sidebar.write("Fine-tuning started successfully!")
+        else:
+            st.sidebar.write("Error starting fine-tuning. Please check your dataset and API key.")
+    else:
+        st.sidebar.warning("Ensure you've uploaded a dataset and provided a valid API key!")
 
 def generate_response(input_text):
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
+    # Assuming OpenAI() works similar to openai.Completion.create
+    response = openai.Completion.create(model="fine-tuned-model-id-if-any", prompt=input_text)
+    st.info(response.choices[0].text)
 
 with st.form('my_form'):
     text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
