66c79011d53c88f51e84d64accfa787f4b17b757
Brian Brinkman <brian@streamcreative.com>
2023-08-23 12:44:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,37 @@
 import streamlit as st
 import openai
+import os
+
+# Set the OpenAI API key
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # App title
 st.title('SC Starter Kit App with Fine-Tuning')
 
-# Check for OpenAI API Key in Streamlit's secrets
-if 'OPENAI_API_KEY' in st.secrets:
-    st.sidebar.success('API key successfully loaded from secrets!')
-    openai_api_key = st.secrets['OPENAI_API_KEY']
+# Check if API key is set (optional but useful for debugging)
+if not openai.api_key:
+    st.sidebar.warning("OpenAI API key is not set!")
 else:
-    openai_api_key = st.sidebar.text_input('Enter OpenAI API Key:', type='password')
-    if not openai_api_key.startswith('sk-'):
-        st.sidebar.warning('Please enter your OpenAI API key!')
+    st.sidebar.success('API key successfully loaded!')
 
 # File Uploader for Fine-Tuning
 uploaded_file = st.sidebar.file_uploader("Upload your training dataset (in .jsonl format)", type="jsonl")
 
 # Fine-Tuning Button
 if st.sidebar.button('Start Fine-Tuning'):
-    if uploaded_file and openai_api_key.startswith('sk-'):
+    if uploaded_file:
         # Upload the Dataset to OpenAI
         uploaded = openai.File.create(file=uploaded_file, purpose='fine-tune')
         
         # Start the Fine-Tuning Job
-        openai.api_key = openai_api_key
         response = openai.FineTuningJob.create(training_file=uploaded.id, model="gpt-3.5-turbo")
         
         if response:
             st.sidebar.write("Fine-tuning started successfully!")
         else:
-            st.sidebar.write("Error starting fine-tuning. Please check your dataset and API key.")
+            st.sidebar.write("Error starting fine-tuning. Please check your dataset.")
     else:
-        st.sidebar.warning("Ensure you've uploaded a dataset and provided a valid API key!")
+        st.sidebar.warning("Ensure you've uploaded a dataset!")
 
 def generate_response(input_text):
     # Assuming OpenAI() works similar to openai.Completion.create
@@ -41,5 +41,5 @@ def generate_response(input_text):
 with st.form('my_form'):
     text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
     submitted = st.form_submit_button('Submit')
-    if submitted and openai_api_key.startswith('sk-'):
+    if submitted:
         generate_response(text)
